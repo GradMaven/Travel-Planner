c95c4c200bddeab93b73e35eec018a8bc567c041
Leonard Ondigo <109470807+GradMaven@users.noreply.github.com>
2025-03-29 15:27:05 -0400

    Update HeroSection.jsx

--- trip-eazzy-app/src/components/HeroSection.jsx
+++ trip-eazzy-app/src/components/HeroSection.jsx
@@ -4,20 +4,19 @@ import React from 'react';
 const HeroSection = () => {
   return (
     <div
-      className="relative h-screen bg-[url('/public/images/herosection background.jpg')] bg-cover bg-center flex items-center justify-center"
-      
-    >
-      {/* Overlay to darken the background image */}
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-
-      <div className="relative z-10 text-center text-white">
-        <h1 className="text-5xl font-bold mb-4">
-          Make Easy All your Travel plans with All-In-One Travel App
+    style={{ backgroundImage: "url('/images/herosection-background.jpg')" }}
+    className="relative min-h-screen bg-cover bg-center bg-no-repeat flex items-center 
+    justify-center text-center px-4 pb-0"
+     >
+ 
+      <div className="relative z-10 text-center text-white w-180">
+        <h1 className="text-5xl font-bold mb-6 mt-16">
+          Make Easy All your Travel plans with <span className='text-[#BF6A02]'>All-In-One Travel App </span>
         </h1>
         <p className="text-xl mb-8">
           Explore the world with us and create unforgettable memories.
         </p>
-        <button className="bg-blue-600 text-white px-8 py-3 rounded-lg text-lg font-semibold hover:bg-blue-700 transition duration-300">
+        <button className="bg-[#03023C] text-white px-8 py-3 rounded-lg text-lg font-semibold hover:bg-[#BF6A02] transition duration-300">
           Let's Get Started
         </button>
       </div>
