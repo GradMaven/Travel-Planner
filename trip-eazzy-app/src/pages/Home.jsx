855a51c7fff2284ca6c00467edb3a3c5942e73ad
leonardondigo <gradmaven@gmail.com>
2025-04-04 09:21:34 -0400

    Resolved merge conflicts

--- trip-eazzy-app/src/pages/Home.jsx
+++ trip-eazzy-app/src/pages/Home.jsx
@@ -1,30 +1,20 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 import Card from '../components/Card';
-<<<<<<< HEAD
 import CardPlanner from '../components/CardPlanner';
 import GetApp from '../components/GetApp';
 import Footer from '../components/Footer';
-=======
->>>>>>> 390f080ff0a63be64e1a323dfdafb170667482b3
-
 
 const Home = () => {
-    return (
-        <>
-       
+  return (
+    <>
       <HeroSection />
       <Card />
-<<<<<<< HEAD
       <CardPlanner />
       <GetApp />
       <Footer />
-=======
->>>>>>> 390f080ff0a63be64e1a323dfdafb170667482b3
-      
-        
-        </>
-    );
+    </>
+  );
 }
 
 export default Home;
