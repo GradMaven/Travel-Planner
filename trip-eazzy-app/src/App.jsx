8a67724735b701c9547a50485b5cea68cd3df16d
leonardondigo <gradmaven@gmail.com>
2025-04-04 09:18:11 -0400

    Fix merge conflict and cleaned App.jsx

--- trip-eazzy-app/src/App.jsx
+++ trip-eazzy-app/src/App.jsx
@@ -1,61 +1,39 @@
-<<<<<<< HEAD
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 
-=======
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 
->>>>>>> 390f080ff0a63be64e1a323dfdafb170667482b3
-'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Home from './pages/Home';
 import "./App.css";
-<<<<<<< HEAD
 import Itinerary from './components/plan/Itinerary';
 import Accommodation from './components/plan/Accommodation';
 import UserProfile from './components/UserProfile';
-=======
 
->>>>>>> 390f080ff0a63be64e1a323dfdafb170667482b3
-
-
-
-function Layout () {
+function Layout() {
   const location = useLocation();
 
   return (
     <>
-<<<<<<< HEAD
-  
-    {location.pathname !== "/login" && <Navbar />}
-   
-=======
-    <Router>
-      <Navbar />
->>>>>>> 390f080ff0a63be64e1a323dfdafb170667482b3
+      {location.pathname !== "/login" && <Navbar />}
+
       <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-<<<<<<< HEAD
-        <Route path ="/userprofile" element= {<UserProfile />}/>
-        <Route path ="/itinerary" element = {<Itinerary />} />
-        <Route path ="/accommodation" element = {<Accommodation />} />
-    </Routes >  
- 
-=======
-    </Routes >  
-    </Router>
->>>>>>> 390f080ff0a63be64e1a323dfdafb170667482b3
+        <Route path="/userprofile" element={<UserProfile />} />
+        <Route path="/itinerary" element={<Itinerary />} />
+        <Route path="/accommodation" element={<Accommodation />} />
+      </Routes>
     </>
-      
-  )
+  );
 }
 
 function App() {
-  return <Layout />
+  return (
+    <Router>
+      <Layout />
+    </Router>
+  );
 }
 
-export default App
+export default App;
